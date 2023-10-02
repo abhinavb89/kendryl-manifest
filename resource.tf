c2259d857ac903c7011155471ba5c043bfa20e0b
gopal <gopal1409@gmail.com>
2023-10-02 14:59:35 +0530

    terraform with resource

--- resource.tf
+++ resource.tf
@@ -1,11 +1,12 @@
 resource "azurerm_resource_group" "myrg1" {
-  name = "myrg-1"
-  location = "East Us"
+  name = "${local.resurce_name_prefix}-${var.resource_group_name}"
+  #sap-dev-rg-default
+  location = var.resource_group_location
 }
-
+/*
 resource "azurerm_resource_group" "myrg2" {
   name = "myrg-3"
   location = "West Us"
   provider = azurerm.provider2-westus
   ###provider.alias name
-}
+}*/
